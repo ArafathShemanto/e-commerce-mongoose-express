7e036a11684c519d7369c5265fd75ee1c5a3d65a
ArafathShemanto <arafathh.shemanto@gmail.com>
2024-05-21 03:05:36 +0600

    add

--- src/app/modules/products/product.controllers.ts
+++ src/app/modules/products/product.controllers.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express"
 import { ProductServices } from "./product.service"
+import mongoose from "mongoose";
 
 
 const createProduct = async (req: Request, res: Response) => {
@@ -17,6 +18,7 @@ const createProduct = async (req: Request, res: Response) => {
     }
 }
 
+// GET ALL PRODUCTS 
 const getProducts = async (req: Request, res: Response) => {
     try {
         const product = req.body;
@@ -32,7 +34,69 @@ const getProducts = async (req: Request, res: Response) => {
     }
 }
 
+// GET SINGLE DATA BY ID
+const getProductByID = async (req: Request, res: Response) => {
+    try {
+        const { productId } = req.params;
+        console.log(productId, "productId")
+
+        if (!productId) {
+            return res.status(400).json({
+                success: false,
+                message: 'Product ID is required.',
+            });
+        }
+
+        const result = await ProductServices.getProductByIDFromDB(productId)
+
+        // if product not found 
+        if (!result) {
+            return res.status(404).json({
+                success: false,
+                message: 'Product not found.',
+            });
+        }
+        res.status(200).json({
+            "success": true,
+            "message": "Products fetched successfully!",
+            "data": result
+        })
+
+    } catch (error: any) {
+        // Handle specific Error
+        if (error instanceof mongoose?.Error?.CastError) {
+            return res.status(400).json({
+                success: false,
+                message: 'Invalid product ID format.',
+            });
+        }
+
+        // Handle all other errors
+        res.status(500).json({
+            success: false,
+            message: 'An error occurred while fetching the product.',
+        });
+    }
+}
+
+// DELETE A PRODUCT BY ID 
+const deleteProductByID = async (req: Request, res: Response) => {
+    try {
+        const { productId } = req.params;
+        const result = await ProductServices.deleteProductByIDFromDB(productId)
+        res.status(200).json({
+            "success": true,
+            "message": "Product Deleted successfully!",
+            "data": null
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 export const productController = {
     createProduct,
-    getProducts
+    getProducts,
+    getProductByID,
+    deleteProductByID
 }
--- src/app/modules/products/product.service.ts
+++ src/app/modules/products/product.service.ts
@@ -2,16 +2,29 @@ import { IProduct } from "./product.interface"
 import { ProductModel } from "./product.model"
 
 const getProductsFromDB = async () => {
-    const result = ProductModel.find()
+    const result = await ProductModel.find()
     return result
 }
 const createProductIntoDB = async (data: IProduct) => {
-    const result = ProductModel.create(data)
+    const result = await ProductModel.create(data)
+    return result
+}
+const getProductByIDFromDB = async (productId: string) => {
+    console.log(productId, "productId is that")
+    const result = await ProductModel.findById({ _id: productId })
     return result
 }
 
+const deleteProductByIDFromDB = async (productId: string) => {
+    console.log(productId, "productId is that")
+    const result = await ProductModel.findByIdAndDelete({ _id: productId })
+    console.log(result, "lol result")
+    return result
+}
 
 export const ProductServices = {
     createProductIntoDB,
-    getProductsFromDB
+    getProductsFromDB,
+    getProductByIDFromDB,
+    deleteProductByIDFromDB
 }
