0ca2be02b510cc21720bdb5df29797732e123f9b
ArafathShemanto <arafathh.shemanto@gmail.com>
2024-05-21 00:54:47 +0600

    server setup

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import { Request, Response } from "express"
-
+import cors from "cors"
 const express = require('express')
 const app = express()
 
+// PERCERS HERE 
+app.use(express.json());
+app.use(cors())
 
+
+// ALL ROUTE HERE 
 app.get('/', (req: Request, res: Response) => {
-    console.log(req)
+    console.log("hei bro")
     res.send('Hello World!')
 })
 
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,16 @@
 import app from "./app"
 import config from "./config"
+const mongoose = require('mongoose');
 
-app.listen(config.port, () => {
-    console.log(`Example app listening on port ${config.port}`)
-})
+
+async function main() {
+    try {
+        await mongoose.connect(config.database_url);
+        app.listen(config.port, () => {
+            console.log(`Example app listening on port ${config.port}`)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+main()
